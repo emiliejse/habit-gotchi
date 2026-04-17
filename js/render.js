4d56a48eaefdcf7d71a99ceb5ecfb70a4e40510a
emiliejse <emilie.josse@protonmail.com>
2026-04-17 13:38:12 +0200

    p.fill('#fff'); p.rect(x+PX*2+1, y+PX*4+1, 4, 4); p.rect(x+PX*6+1, y+PX*4+1, 4, 4);

--- js/render.js
+++ js/render.js
@@ -269,7 +269,7 @@ function drawAdult(p, cx, cy, sl, en, ha) {
       p.fill(C.eye); px(p,x+PX*2,y+PX*5,PX*2,PX); px(p,x+PX*6,y+PX*5,PX*2,PX); 
     } else { 
       p.fill(C.eye); px(p,x+PX*2,y+PX*4,PX*2,PX*2); px(p,x+PX*6,y+PX*4,PX*2,PX*2); 
-      p.fill('#fff'); px(p,x+PX*2,y+PX*4,PX,PX*2); px(p,x+PX*6,y+PX*4,PX,PX*2);
+      p.fill('#fff'); p.rect(x+PX*2+1, y+PX*4+1, 4, 4); p.rect(x+PX*6+1, y+PX*4+1, 4, 4);
     }
 
     p.fill(C.cheek); px(p,x+PX,y+PX*6,PX,PX); px(p,x+PX*8,y+PX*6,PX,PX);

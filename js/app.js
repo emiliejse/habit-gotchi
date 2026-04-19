1fed77b4aa44f61886c077f3de634d72cc536d41
emiliejse <emilie.josse@protonmail.com>
2026-04-19 17:38:28 +0200

    window.APP_VERSION = 'v1.4

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ window.meteoData  = null;
 
 
 // VERSION À CHANGER
-window.APP_VERSION = 'v1.2'; // // ⚠️ SYNC → sw.js ligne 1 : CACHE_VERSION
+window.APP_VERSION = 'v1.4'; // // ⚠️ SYNC → sw.js ligne 1 : CACHE_VERSION
 
 
 /* ============================================================

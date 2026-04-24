8423c394552ded9568c6200f55914d304c10781b
emiliejse <emilie.josse@protonmail.com>
2026-04-24 21:01:54 +0200

    v2.43

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    pour forcer le rechargement chez tous les utilisateurs.
    ============================================================ */
 
-const CACHE_VERSION = 'hg-v2.42';  // ⚠️ SYNC → app.js ligne 1 : window.APP_VERSION
+const CACHE_VERSION = 'hg-v2.43';  // ⚠️ SYNC → app.js ligne 1 : window.APP_VERSION
 
 const ASSETS = [
   './',

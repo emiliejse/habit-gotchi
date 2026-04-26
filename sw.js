5a05ee229dbf7975a928fe59afe7922f7ae14500
emiliejse <emilie.josse@protonmail.com>
2026-04-26 20:48:24 +0200

    v3.20

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    pour forcer le rechargement chez tous les utilisateurs.
    ============================================================ */
 
-const CACHE_VERSION = 'hg-v3.19';  // ⚠️ SYNC → app.js ligne 1 : window.APP_VERSION
+const CACHE_VERSION = 'hg-v3.20';  // ⚠️ SYNC → app.js ligne 1 : window.APP_VERSION
 
 const ASSETS = [
   './',

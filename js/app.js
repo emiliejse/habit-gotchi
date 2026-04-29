c320f717360510a31a60c071083ef200d9945391
Émilie Josse <emilie.josse@protonmail.com>
2026-04-29 20:25:31 +0200

    v4.41

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ window._gotchiActif = true;
 
 
 // VERSION À CHANGER
-window.APP_VERSION = 'v4.4'; // // ⚠️ SYNC → sw.js ligne 1 : CACHE_VERSION
+window.APP_VERSION = 'v4.41'; // // ⚠️ SYNC → sw.js ligne 1 : CACHE_VERSION
 
 // Limites journal (S6 — Introspection)
 window.JOURNAL_MAX_PER_DAY = 5;

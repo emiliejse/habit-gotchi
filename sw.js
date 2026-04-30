9247aee5539d34c2414413be7092368ce85018ec
Émilie Josse <emilie.josse@protonmail.com>
2026-04-30 21:40:02 +0200

    v4.8

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    pour forcer le rechargement chez tous les utilisateurs.
    ============================================================ */
 
-const CACHE_VERSION = 'v4.75';  // ⚠️ SYNC → app.js ligne 1 : window.APP_VERSION
+const CACHE_VERSION = 'v4.8';  // ⚠️ SYNC → app.js ligne 1 : window.APP_VERSION
 
 const ASSETS = [
   './',
